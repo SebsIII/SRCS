1f63050694c2da38cb4d3541cb11606719da0ddd
Sebs_ <144163569+SebsIII@users.noreply.github.com>
2025-03-24 18:00:53 +0100

    Add files via upload

---
+++ snippets/eth/eth.ino
@@ -0,0 +1,107 @@
+/*
+  SRCS Web Server stock
+  based on Arduino built-in example Web server examples
+
+  ETH SHIELD -> STM32
+  
+     SCK MISO
+      | |
+  [ - - -] ICSP INTERFACE
+  [ - - -]
+      |
+    MOSI
+  
+  MISO -> D12
+  MOSI -> D11
+  SCK -> D13
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 100);   //static IP 
+
+EthernetServer server(80);
+
+void setup() {
+  Ethernet.init(10);    //CS PIN
+
+  Serial.begin(9600);
+  Serial.println("SRCS-Web-Server");
+
+  Ethernet.begin(mac, ip);
+
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.");
+    while (true) {
+      delay(100);
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+
+          client.println("<body style=background-color:gray;display:flex;align-items:center;justify-content:center;flex-direction:column;>");
+          
+          client.println("<p style='color:white;font-size:20vw;margin:0px;' > SRCS </p>");
+          client.println("<h3 style='color:white;margin:0px;' > Main Dashboard </h3>");
+          client.println("<button onclick=X()>Test</button>");
+
+          client.println("<script>function X(){document.querySelector('body').style.backgroundColor = 'blue'}</script>");
+
+          client.println("</body>");
+
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println(client.connected());
+    Serial.println("client disconnected");
+  }
+}
+
