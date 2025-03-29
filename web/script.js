9c3a118e523bef8f07ec1aa7b39103dcd5083572
Sebs_ <144163569+SebsIII@users.noreply.github.com>
2025-03-29 08:57:34 +0100

    Add files via upload

--- web/script.js
+++
@@ -1,7 +0,0 @@
-async function x () {
-    const response = await fetch("https://api.n2yo.com/rest/v1/satellite/tle/25544&apiKey=589P8Q-SDRYX8-L842ZD-5Z9");
-    const data = await response.text();
-    console.log(data);
-}
-
-x()
