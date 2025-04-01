b86437f318f69d4958ed298841b70a805fe401c3
Sebs_ <144163569+SebsIII@users.noreply.github.com>
2025-04-02 00:02:20 +0200

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -4,10 +4,20 @@ function getDate(){
     return currentDate
 }
 
+const time_text =  document.getElementById("day-time-text")
+
 let options = { day: 'numeric', month: 'numeric', year: 'numeric' };
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 let timeGod = getDate()
+let dayIndex = timeGod.getDay()
 
 document.getElementById("weather-status").innerText = "Raining"
 document.getElementById("date-text").innerText = timeGod.toLocaleDateString('it-IT', options);
-document.getElementById("day-time-text").innerText = weekday[timeGod.getDay()] + " - " + timeGod.getHours() + ":" + timeGod.getMinutes()
+function updateTime(){
+    time_text.innerText = weekday[dayIndex] + " - " + getDate().getHours() + ":" + getDate().getMinutes()
+}
+updateTime()
+
+setInterval(updateTime, 15000)      //update time every 35 sec
+
+document.getElementById("temperature-status").innerText = "13"
