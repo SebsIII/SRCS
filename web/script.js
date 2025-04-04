497edb7b7340c9b033ec8296e4ed2bba64d1f891
Sebs_ <144163569+SebsIII@users.noreply.github.com>
2025-04-04 12:53:41 +0200

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,40 @@
+function getDate(){
+    let currentDate = new Date();
+    currentDate.getDay
+    return currentDate
+}
+
+const time_text =  document.getElementById("day-time-text")
+const main = document.querySelector("main")
+const body = document.querySelector("body")
+const NOAA_div = document.getElementById("NOAA-div")
+
+let options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let timeGod = getDate()
+let dayIndex = timeGod.getDay()
+
+body.style.backgroundImage = "url('img/cloudy.webp')";
+
+document.getElementById("weather-status").innerText = "Raining"
+document.getElementById("date-text").innerText = timeGod.toLocaleDateString('it-IT', options);
+function updateTime(){
+    time_text.innerText = weekday[dayIndex] + " - " + getDate().getHours() + ":" + getDate().getMinutes()
+}
+updateTime()
+
+setInterval(updateTime, 15000)      //update time every 35 sec
+
+document.getElementById("temperature-status").innerText = "13"
+document.getElementById("humidity-status").innerText = "67"
+document.getElementById("light-status").innerText = "600"
+document.getElementById("recap-text").innerText = "The continuous downpour throughout the morning led to challenging conditions for commuters, as the wet and slippery roads required drivers to exercise increased caution. Pedestrians, too, navigated the streets carefully, using umbrellas and raincoats to shield themselves from the relentless rainfall."
+
+NOAA_div.addEventListener("click",() => {
+    main.style.opacity = "0%"
+    main.style.transform = "TranslateX(-100vw)"
+    NOAA_div.style.transform = "TranslateX(-100vw)"
+    setTimeout(()=>{main.style.display = "none"}, 700)
+})
+
+//fetch api from srcs
