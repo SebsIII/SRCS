2909e1a74b9a1122a11123637a11961117cef2eb
Sebs_ <144163569+SebsIII@users.noreply.github.com>
2025-04-15 10:50:20 +0200

    fixed some bugs, added features made new functions

---
+++ web-local/WEBSERVER.ino
@@ -0,0 +1,153 @@
+//-----------------------------------------
+//Arduino DHT11 Web Server using AJAX
+//HTML code of webpage is stored on SD card
+//-----------------------------------------
+#include <SPI.h>
+#include <Ethernet.h>
+#include <SD.h>
+#include <DHT.h>
+//------------------------------------------------
+
+#define DHTPIN 3
+#define DHTTYPE DHT11
+
+#define PHOTOPIN A0 //?
+#define RAINPIN A1 //?
+
+DHT dht(DHTPIN, DHTTYPE);
+File HMTL_file;
+//------------------------------------------------
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 100); 
+EthernetServer server(80);
+//==================================================================================
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(PHOTOPIN, INPUT);
+  //-----------------------------------------------------
+  Serial.println(F("Initializing SD card..."));
+  if(!SD.begin(4))
+  {
+    Serial.println(F("Initialization failed!")); return;
+  }
+  Serial.println(F("Initialization OK"));
+  //-----------------------------------------------------
+  if(!SD.exists("index.htm"))
+  {
+    Serial.println(F("index.htm not found!")); return;
+  }
+  Serial.println(F("index.htm found"));
+  //-----------------------------------------------------
+  Ethernet.init(10);
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print(F("Server Started...\nLocal IP: "));
+  Serial.println(Ethernet.localIP());
+}
+//==================================================================================
+void loop()
+{
+  String HTTP_req;
+  EthernetClient client = server.available();
+  //---------------------------------------------------------------------------
+  if(client)
+  {
+    boolean currentLineIsBlank = true;
+    while (client.connected())
+    {
+      if(client.available())
+      { 
+        char c = client.read();
+        HTTP_req += c;
+        if(c == '\n' && currentLineIsBlank)
+        {
+          //-------------------------------------------------------------------
+          if(HTTP_req.indexOf("readT")>-1)
+          {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/plain");
+              client.println("Connection: close");
+              client.println();
+              client.println(getTemperature());
+          }
+          else if(HTTP_req.indexOf("readH")>-1)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/plain");
+            client.println("Connection: close");
+            client.println();
+            client.println(getHumidity());
+          }
+          else if(HTTP_req.indexOf("readL")>-1)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/plain");
+            client.println("Connection: close");
+            client.println();
+            client.println(getLight());
+          }
+          else if(HTTP_req.indexOf("readL")>-1)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/plain");
+            client.println("Connection: close");
+            client.println();
+            client.println(getRain());
+          }
+          //-------------------------------------------------------------------
+          else
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            HMTL_file = SD.open("index.htm");
+            if(HMTL_file)
+            {
+              while(HMTL_file.available()) client.write(HMTL_file.read());
+              HMTL_file.close();
+            }
+          }
+          //-------------------------------------------------------------------
+          //HTTP_req = "";
+          break;
+        }
+        //---------------------------------------------------------------------
+        if(c == '\n') currentLineIsBlank = true;
+        else if(c != '\r') currentLineIsBlank = false;
+      }
+    }
+    delay(10);
+    client.stop();
+  }
+}
+
+float getTemperature(){
+  float T = dht.readTemperature();
+  if (isnan(T)) {   
+    return 0;
+  }
+  return  T;
+}
+
+float getHumidity(){
+  float H = dht.readHumidity();
+  if (isnan(H)) {   
+    return 0;
+  }
+  return  H;
+}
+
+int getLight(){  //To format raw input 
+  int LL = analogRead(PHOTOPIN)
+  return  LL;
+}
+
+int getRain(){  //To format raw input 
+  int RL = analogRead(RAINPIN)
+  return  RL;
+}
--- web-local/script.js
+++ web-local/script.js
@@ -1,4 +1,101 @@
+
 document.getElementById("time-left").innerText = "10:03"
 
-document.getElementById("temperature-var").innerText = 32
-document.getElementById("humidity-var").innerText = 100
+//document.getElementById("temperature-var").innerText = 32
+
+/*
+setInterval(function()
+{
+    getTemperature();
+    getHumidity();
+    getLight();
+    getRain();
+}, 2000);
+*/
+
+function getTemperature()
+{
+    var DHT11Request = new XMLHttpRequest();
+    DHT11Request.onreadystatechange = function()
+    {
+    if(this.readyState == 4 && this.status == 200 && this.responseText != null)
+    {
+        document.getElementById("temperature-var").innerHTML = this.responseText;
+    }
+    };
+    DHT11Request.open("GET", "readT", true);
+    DHT11Request.send();
+}
+
+function getHumidity()
+{
+    var DHT11Request = new XMLHttpRequest();
+    DHT11Request.onreadystatechange = function()
+    {
+    if(this.readyState == 4 && this.status == 200 && this.responseText != null)
+    {
+        document.getElementById("humidity-var").innerHTML = this.responseText;
+    }
+    };
+    DHT11Request.open("GET", "readH", true);
+    DHT11Request.send();
+}
+
+function getLight(){
+    var DHT11Request = new XMLHttpRequest();
+    DHT11Request.onreadystatechange = function()
+    {
+    if(this.readyState == 4 && this.status == 200 && this.responseText != null)
+    {
+        document.getElementById("light-var").innerHTML = this.responseText;
+    }
+    };
+    DHT11Request.open("GET", "readL", true);
+    DHT11Request.send();
+}
+
+function getRain(){
+    var DHT11Request = new XMLHttpRequest();
+    DHT11Request.onreadystatechange = function()
+    {
+    if(this.readyState == 4 && this.status == 200 && this.responseText != null)
+    {
+        document.getElementById("rain-var").innerHTML = this.responseText;
+    }
+    };
+    DHT11Request.open("GET", "readR", true);
+    DHT11Request.send();
+}
+
+function getSRCS(){
+    var DHT11Request = new XMLHttpRequest();
+    DHT11Request.onreadystatechange = function()
+    {
+    if(this.readyState == 4 && this.status == 200 && this.responseText != null)
+    {
+        document.getElementById("POST-var").innerHTML = this.responseText;
+    }
+    };
+    DHT11Request.open("GET", "readSRCS", true);
+    DHT11Request.send();
+}
+
+function getGeneral(){
+    var DHT11Request = new XMLHttpRequest();
+    DHT11Request.onreadystatechange = function()
+    {
+    if(this.readyState == 4 && this.status == 200 && this.responseText != null)
+    {
+        //document.getElementById("POST-var").innerHTML = this.responseText;
+        //format
+    }
+    };
+    DHT11Request.open("GET", "readGENERAL", true);
+    DHT11Request.send();
+}
+
+function updateN2YO(){
+    //fetch from n2yo api
+    console.log("wait a sec, not done yet")
+    return
+}
