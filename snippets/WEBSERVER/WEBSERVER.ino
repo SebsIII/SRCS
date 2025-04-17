81cf50ddd4b9efc7c2a151ce0fc95f34ab20b628
Sebs_ <144163569+SebsIII@users.noreply.github.com>
2025-04-17 19:50:31 +0200

    Add files via upload

--- snippets/WEBSERVER/WEBSERVER.ino
+++ snippets/WEBSERVER/WEBSERVER.ino
@@ -11,6 +11,10 @@
 #define DHTPIN 3
 #define DHTTYPE DHT11
 
+#define PHOTOPIN A0
+#define RAINPIN A1 
+#define ANEMOPIN A2
+
 DHT dht(DHTPIN, DHTTYPE);
 File HMTL_file;
 //------------------------------------------------
@@ -22,27 +26,14 @@ EthernetServer server(80);
 //==================================================================================
 void setup()
 {
-  Serial.begin(9600);
-  dht.begin();
-  //-----------------------------------------------------
-  Serial.println(F("Initializing SD card..."));
-  if(!SD.begin(4))
-  {
-    Serial.println(F("Initialization failed!")); return;
+  //Serial.begin(9600);
+  pinMode(PHOTOPIN, INPUT);
+  pinMode(RAINPIN, INPUT);
+  pinMode(ANEMOPIN, INPUT);
+  if(!POST()){
+    while (true){};
   }
-  Serial.println(F("Initialization OK"));
-  //-----------------------------------------------------
-  if(!SD.exists("index.htm"))
-  {
-    Serial.println(F("index.htm not found!")); return;
-  }
-  Serial.println(F("index.htm found"));
-  //-----------------------------------------------------
-  Ethernet.init(10);
-  Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print(F("Server Started...\nLocal IP: "));
-  Serial.println(Ethernet.localIP());
+  
 }
 //==================================================================================
 void loop()
@@ -68,7 +59,7 @@ void loop()
               client.println("Content-Type: text/plain");
               client.println("Connection: close");
               client.println();
-              client.println(dht.readTemperature());
+              client.println(getTemperature());
           }
           else if(HTTP_req.indexOf("readH")>-1)
           {
@@ -76,7 +67,31 @@ void loop()
             client.println("Content-Type: text/plain");
             client.println("Connection: close");
             client.println();
-            client.println(dht.readHumidity());
+            client.println(getHumidity());
+          }
+          else if(HTTP_req.indexOf("readL")>-1)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/plain");
+            client.println("Connection: close");
+            client.println();
+            client.println(getLight());
+          }
+          else if(HTTP_req.indexOf("readR")>-1)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/plain");
+            client.println("Connection: close");
+            client.println();
+            client.println(getRain());
+          }
+          else if(HTTP_req.indexOf("readW")>-1)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/plain");
+            client.println("Connection: close");
+            client.println();
+            client.println(getWind());
           }
           //-------------------------------------------------------------------
           else
@@ -103,5 +118,53 @@ void loop()
     }
     delay(10);
     client.stop();
+  }
+}
+
+float getTemperature(){
+  float T = dht.readTemperature();
+  if (isnan(T)) {   
+    return 0;
+  }
+  return  T;
+}
+
+float getHumidity(){
+  float H = dht.readHumidity();
+  if (isnan(H)) {   
+    return 0;
+  }
+  return  H;
+}
+
+int getLight(){  //To format raw input 
+  int LL = analogRead(PHOTOPIN);
+  return  LL;
+}
+
+int getRain(){  //To format raw input 
+  int RL = analogRead(RAINPIN);
+  return  RL;
+}
+
+float getWind(){  //To format raw input 
+  int WS = analogRead(ANEMOPIN);
+  return  WS;
+}
+
+bool POST(){
+  if(SD.begin(4)){
+    if(SD.exists("index.htm")){
+      Ethernet.init(10);
+      Ethernet.begin(mac, ip);
+      server.begin();
+      //-----------------------------
+      dht.begin();
+      return true;
+    } else{
+      return false;
+    }
+  } else{
+    return false;
   }
 }
